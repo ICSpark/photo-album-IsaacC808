1ee4f00a739625ff3213d58e15e69331dae3f62b
IsaacC808 <isaac808hba@gmail.com>
2025-03-08 21:23:24 +0000

    creating elements

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 function fix() {
-    //Write your code here!
+    let coconut = document.getElementById("coconut");
+    coconut.remove("coconut");
+    
+    let coast = document.getElementById("coast");
+    coast.remove(coast);
+
+    let watermeleon = document.getElementById("watermelon");
+    watermeleon.remove(watermelon);
+
+    let sunflowers = document.getElementById("sunflowers");
+    sunflowers.remove(sunflowers);
+
+    let tree = document.createElement("img");
+    tree.id = "tree";
+    tree.src = "to-add/christmasTree.jpg";
+    let parent = document.getElementById("photos");
+    parent.appendChild(tree);
+
+    let snowmen = document.createElement("img");
+    snowmen.id = "snowmen";
+    snowmen.src = "to-add/snowmen.jpg";
+    parent.appendChild(snowmen);
+
+    let stockings = document.createElement("img");
+    stockings.id = "stockings";
+    stockings.src = "to-add/stockings.jpg";
+    parent.appendChild(stockings);
+
+   
+
+   
+
 }
